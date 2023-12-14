fa7ade2a7781eead3bf8c3c82560cf8274a22a2b
Alexandr Pobezhimov <Alexandr@managility.com>
2023-12-15 00:10:02 +0300

    Added mock backend as temporary solution

--- src/components/api/api.ts
+++ src/components/api/api.ts
@@ -1,41 +1,101 @@
 import axios from "axios";
 
-interface UserData {
+const mockApiUrl = "http://localhost:3001/api"; // Temporary solution
+const apiUrl = "https://localhost:5001/api";
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface LoginResponse {
+  message: string;
+}
+
+export interface UserDataResponse {
   firstName: string;
   balance: number;
 }
 
-interface UserCredentialsCheckResult {
-  isExist: boolean;
-  isAuthorized: boolean;
+export interface TransactionRequest {
+  amount: number;
+  recipientEmail: string;
+}
+
+export interface TransactionResponse {
+  transactionStatus: string;
+  transactionMessage: string;
+  newBalance: number;
+}
+
+export interface Transaction {
+  id: number;
+  date: string;
+  sum: number;
+  sender: string;
+  receiver: string;
+}
+
+export interface TransactionHistoryRequest {
+  page: number;
+}
+
+export interface TransactionHistoryResponse {
+  transactions: Transaction[];
+  totalPages: number;
 }
 
 // TODO: Update after UserController refactoring - add body
-export const fetchUserData = async (): Promise<UserData> => {
+export const fetchUserData = async (): Promise<UserDataResponse> => {
   try {
-    const response = await axios.get("https://localhost:5001/api/User/1");
+    const response = await axios.get(`${mockApiUrl}/User`);
     return response.data;
-  } catch (error) {
-    throw new Error("Error fetching user data");
+  } catch (error: any) {
+    throw new Error(`Error fetching user data: ${error.message}`);
   }
 };
 
-// TODO #1: Return that API call to
-// TODO #2: Send encrypted user password after DB schema fix
-export const checkUserCredentials =
-  async (): Promise<UserCredentialsCheckResult> => {
-    try {
-      const userCredentials = {
-        userId: 1,
-        userEmail: "johndoe@example.com",
-        userPassword: "testpassword",
-      };
-      const response = await axios.post(
-        "https://localhost:5001/api/User/CheckCredentials",
-        userCredentials
-      );
-      return response.data;
-    } catch (error) {
-      throw new Error("Error with user credentials check");
+// TODO #1: Send encrypted user password after DB schema fix
+export const loginUser = async (
+  credentials: LoginRequest
+): Promise<LoginResponse> => {
+  try {
+    const response = await axios.post(`${mockApiUrl}/Login`, credentials);
+
+    if (response.status === 200) {
+      return { message: response.data.message };
+    } else {
+      throw new Error("Authentication failed. Please check your credentials.");
     }
-  };
+  } catch (error) {
+    throw new Error("Authentication failed. Please try again later.");
+  }
+};
+
+export const fetchTransactionHistory = async (
+  request: TransactionHistoryRequest
+): Promise<TransactionHistoryResponse> => {
+  try {
+    const response = await axios.post(
+      `${mockApiUrl}/Transactions/History`,
+      request
+    );
+    return response.data;
+  } catch (error: any) {
+    throw new Error(`Error fetching transaction history data: ${error.message}`);
+  }
+};
+
+export const sendMoney = async (
+  data: TransactionRequest
+): Promise<TransactionResponse> => {
+  try {
+    const response = await axios.post(
+      `${mockApiUrl}/Transaction/SendMoney`,
+      data
+    );
+    return response.data;
+  } catch (error: any) {
+    throw new Error(`Error sending money: ${error.message}`);
+  }
+};
---
+++ src/mocks/mockBackendServer.js
@@ -0,0 +1,83 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const port = 3001;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+// Mocked user data
+const mockedUserData = {
+  firstName: 'John',
+  balance: Math.random() * 100,
+};
+
+// Mocked login response
+const mockedLoginResponse = {
+  message: 'Login successful',
+};
+
+// Mocked transaction response
+const mockedTransactionResponse = {
+  transactionStatus: 'success',
+  transactionMessage: 'Transaction completed successfully',
+  newBalance: Math.random() * 100,
+};
+
+// Mocked transaction response
+const generateTransaction = () => ({
+  id: Math.floor(Math.random() * 1000),
+  date: new Date().toUTCString(),
+  sum: Math.random() * 100,
+  sender: 'johndoe@example.com',
+  receiver: 'johndoejunior@example.com',
+});
+
+// Mocked API endpoints
+app.get('/api/User', (req, res) => {
+  res.json(mockedUserData);
+});
+
+app.post('/api/Login', (req, res) => {
+  res.status(200).json(mockedLoginResponse);
+});
+
+app.post('/api/Transaction/SendMoney', (req, res) => {
+  res.json(mockedTransactionResponse);
+});
+
+app.post('/api/Transactions/History', (req, res) => {
+  let transactions = [];
+  
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+  transactions.push(generateTransaction());
+
+  const totalPages = 2;
+  const page = 1;
+  const pageSize = 10;
+  const startIndex = (page - 1) * pageSize;
+  const endIndex = startIndex + pageSize;
+  const paginatedTransactions = transactions.slice(startIndex, endIndex);
+
+  res.json({
+    transactions: paginatedTransactions,
+    totalPages,
+  });
+});
+
+
+app.listen(port, () => {
+  console.log(`Mock server is running at http://localhost:${port}`);
+});
