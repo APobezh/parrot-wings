d8b185f2de2e1efe6a42b7f104b60a90482139d9
Alexandr Pobezhimov <Alexandr@managility.com>
2023-12-15 00:14:13 +0300

    Fixed Login form

--- src/components/auth/Login/Login.tsx
+++ src/components/auth/Login/Login.tsx
@@ -1,37 +1,63 @@
-import React, { useEffect, useState } from "react";
+import { FC, FormEvent, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../../home/common/Button/Button";
-import { checkUserCredentials } from "../../api/api";
+import { useAuth } from "../AuthContext";
+import { loginUser, LoginRequest } from "../../api/api";
 import "./Login.css";
 
-const Login: React.FC = () => {
-  const handleSubmit = (e: React.FormEvent) => {
+const Login: FC = () => {
+  const navigate = useNavigate();
+  const { login } = useAuth();
+
+  const [credentials, setCredentials] = useState<LoginRequest>({
+    email: "",
+    password: "",
+  });
+  const [error, setError] = useState<string | null>(null);
+
+  const handleSubmit = async (e: FormEvent) => {
     e.preventDefault();
-    console.log("Submit Sign In form");
-    // const [credentialsResult, setCredentialsResult] = useState<{ isExist: boolean, isAuthorized: boolean } | null>(null);
 
-    // useEffect(() => {
-    //     const checkCredentials = async () => {
-    //     try {
-    //         const checkCredentialsResult = await checkUserCredentials();();
-    //         setCredentialsResult(checkCredentialsResult);
-    //     } catch (error: any) {
-    //         console.error(error.message);
-    //     }
-    //     };
-
-    //     checkCredentials();
-    // }, []);
+    try {
+      // TODO: Use response message later
+      await loginUser(credentials);
+      login();
+      navigate("/home");
+    } catch (error) {
+      console.error("Error during login:", error);
+      setError("Authentication failed. Please try again later.");
+    }
   };
 
   return (
     <form className="login-form" onSubmit={handleSubmit}>
       <label htmlFor="email">Email:</label>
-      <input type="email" id="email" name="email" required />
+      <input
+        type="email"
+        id="email"
+        name="email"
+        value={credentials.email}
+        onChange={(e) =>
+          setCredentials({ ...credentials, email: e.target.value })
+        }
+        required
+      />
 
       <label htmlFor="password">Password:</label>
-      <input type="password" id="password" name="password" required />
+      <input
+        type="password"
+        id="password"
+        name="password"
+        value={credentials.password}
+        onChange={(e) =>
+          setCredentials({ ...credentials, password: e.target.value })
+        }
+        required
+      />
 
       <Button type="submit">Sign In</Button>
+
+      {error && <p className="error-message">{error}</p>}
     </form>
   );
 };
